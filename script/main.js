28531e0282c00ffe79a5d3b413549136f9a789f6
Denis Petrov-Ploskirev <todenis@inbox.ru>
2020-03-11 02:49:56 +0300

    Modified index.html, added portfolio.html, added temporary main.js

--- script/main.js
+++ script/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let menuBtn = document.querySelector('.menu-btn');
+let closeBtn = document.querySelector('.close-btn');
+let modalMenu = document.querySelector('.mainmenu-wrapper');
+
+menuBtn.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+
+function openMenu() {
+  modalMenu.classList.remove('hidden');
+}
+
+function closeMenu() {
+  modalMenu.classList.add('hidden');
+}
